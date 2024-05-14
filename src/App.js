7129dd97c12f8877b1ab071001bb57305bd1bd9e
gcolley <geremy.colley@cegedim.com>
2024-05-14 17:02:34 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef, useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [counter, setCounter] = useState(0);
+  const myDiv = useRef();
+
+  console.log("render and counter = ", counter);
+
+  useEffect(() => {
+    console.log("Début");
+    const handleClickOutside = (event) => {
+      if (!myDiv.current.contains(event.target)) {
+        console.log("J'ai cliqué à l'extérieur de ma div rouge");
+        wrapper.classList.add('active');
+      }
+    };
+
+    const $ = document;
+    $.addEventListener("click", handleClickOutside);
+    const wrapper = $.querySelector('.my-div');
+    return () => {
+      console.log("Return remove");
+      $.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  return(
+    <>
+      <div className="my-div" ref={myDiv}>
+        <p>je suis la </p>
+        <div>
+          <span>{counter}</span>
+          <button
+            onClick={() => {
+              setCounter(counter + 1);
+            }}
+          >Increment</button>
+        </div>
+      </div>
+      
+    </>
   );
 }
 
